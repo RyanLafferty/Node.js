1ae168906f22f6e68a42a892e859eb7c5c45ec0e
RyanLafferty <rlafferty1@hotmail.com>
2015-05-13 23:56:48 -0400

    completed first node js module
    this is pretty cool :)

---
+++ learnyounode/module1.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = filter;
+
+function filter(dir, ext, callback)
+{
+    fs.readdir(dir, function Read(err, files)
+    {
+        if(err)
+        {
+            callback(err, null);
+        }
+        else
+        {
+            result = [];
+            files.forEach(function (file)
+            {
+                if (path.extname(file) == '.' + ext)
+                {
+                    result.push(file);
+                }
+            });
+
+            callback(null, result);
+        }
+    });
+};
---
+++ learnyounode/program6.js
@@ -0,0 +1,9 @@
+var mod1 = require('./module1.js');
+
+mod1(process.argv[2], process.argv[3], function(err, files)
+{
+    for(i = 0; i < files.length; i++)
+    {
+        console.log(files[i]);
+    }
+});
