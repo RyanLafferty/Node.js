3c821482405be6c43ee8218e49cd4bf44d1b3c70
RyanLafferty <rlafferty1@hotmail.com>
2016-04-05 20:16:18 -0400

    done http client

--- learnyounode/program7.js
+++ learnyounode/program7.js
@@ -6,10 +6,18 @@ var http = require('http');
 
 http.get(process.argv[2], function(response)
 {
-    console.log("Response code: " + response.statusCode);
+//    console.log("Response code: " + response.statusCode);
+    //console.log(response.data);
+response.setEncoding('utf8');
 
-})
-.on('error', function (e)
+response.on('error', function (e)
 {
     console.log("Error message: " + e.message);
+});
+
+response.on('data', function (data)
+{
+    console.log(data);
+});
+
 });
